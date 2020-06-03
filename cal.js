617964d6c22d415fc7c8ff6d94ea8173321463c8
LuanCarvalhoXavier <luancx98@gmail.com>
2020-06-02 21:34:08 -0300

    versão 0.1

---
+++ cal.js
@@ -0,0 +1,19 @@
+function botao( num )
+{
+    var salvo = document.calc.visor.value;
+    document.calc.visor.value = salvo + num;
+    
+}
+
+function reset()
+{
+    // codigo para limpar o visor
+}
+
+function calcule()
+{
+    // Aqui no caso seria o codigo para calcular o visor
+    // depois teria que pegar o valor do visor em uma variável 
+    // em seguida também fazer o cálculo, neste caso com uma das ferramentas do JavaScript
+    // neste caso teria que pegar o resultado e colocar no visor
+}
