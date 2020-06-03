95904a7bba5f067cde59ac2304c32a14daa22602
LuanCarvalhoXavier <luancx98@gmail.com>
2020-06-02 22:45:22 -0300

    versão 0.1

--- cal.js
+++ cal.js
@@ -1,5 +1,11 @@
 function botao( num )
 {
+    //foi necessário criar essa função pois, para que os botões pudessem "ganhar vida", ou seja, na medida
+    //em que fosse clicados, aparecer no visor o número clicado, ou os operadores;
+    //bem como também para haver a concatenação na medida dos cliques em um certo número;
+    //a variável "num" foi para que a mesma seja passada em parâmetro para que no código 
+    // a mesma possa representar em relação ao valor do próprio botão, para não haver repetições do mesmo botão.
+    // para que cada botão tenha uma leitura de valor diferente. ex: "1", '2', '3' etc..
     var salvo = document.calc.visor.value;
     document.calc.visor.value = salvo + num;
     
@@ -7,7 +13,7 @@ function botao( num )
 
 function reset()
 {
-    // codigo para limpar o visor
+    // Aqui no caso seria o código para limpar o visor, más pode perceber qu ele está limpando normalmente
 }
 
 function calcule()
